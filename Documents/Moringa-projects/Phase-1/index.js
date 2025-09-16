2464f70a3518ba7c89fc5b8f2dc1b53c922c965c
mohamedshafi-glitch <mohamed.shafi@student.moringaschool.com>
2025-09-16 20:07:01 +0300

    added code challenge wee-1

---
+++ Documents/Moringa-projects/Phase-1/index.js
@@ -0,0 +1,16 @@
+let person = {
+    name: "Titus",
+    gender: "male",
+    address: {
+        zipcode: "01001",
+        city: "Nairobi"
+    },
+
+    friends: ["Omar", "jackline" ,"Brian", "Doreen"],
+
+    greet: function() {
+        console.log("Hello, Titus");
+    }
+};
+
+console.log(person.Titus);
